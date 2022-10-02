846c75e063dcc475d4132c1098eba4e2ea37a5b5
junseok <wnslgood58@naver.com>
2022-10-02 15:31:54 +0900

    로그인, admin 계정에 데이터베이스 추가
    email 로그인 구현, createsuperuser 에러 해결

--- user/models.py
+++ user/models.py
@@ -9,4 +9,11 @@ class UserModel(AbstractUser):
     class Meta:
         db_table = "my_user" # 여기는 테이블 이름이에요! 꼭 기억 해 주세요!
         
+        
+    email = models.EmailField(verbose_name = "email", max_length = 255, unique = True)
+    username = models.CharField(max_length=30)
     nickname = models.CharField(max_length=50, blank=True)
+    
+    USERNAME_FIELD = 'email' 
+    REQUIRED_FIELDS = ['username']
+    
